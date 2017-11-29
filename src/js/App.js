53137a245249654dc7ec7f71a387073af65b2318
Alexandra Maria Pop <alexandrapop@Alexandras-Mac-mini.local>
2017-11-29 16:37:53 +0200

    Removed useless comment.

--- src/js/App.js
+++ src/js/App.js
@@ -3,14 +3,5 @@ import ReactDOM from "react-dom";
 
 import SliderContainer from "./components/SliderContainer";
 
-// const firstObject = {
-//     containerName: ".custom-slider-container",
-//     sliderWidth: 500,
-//     minimumValue: 0,
-//     maximumValue: 500,
-//     firstHandleValue: 10,
-//     secondHandleValue: 350,
-// };
-
 const app = document.getElementById( "app" );
 ReactDOM.render( <SliderContainer />, app );
